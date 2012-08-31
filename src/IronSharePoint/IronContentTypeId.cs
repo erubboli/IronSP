e3ccfbd17c72128be4cd98debf9be56416408252
Christian Glessner <cg@expertsinside.com>
2012-08-31 11:58:02 +0200

    Clean ups and renamings

--- src/IronSharePoint/IronContentTypeId.cs
+++ src/IronSharePoint/IronContentTypeId.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace IronSharePoint
 {
-    public class IronContentTypeIds
+    public class IronContentTypeId
     {
         public const string IronScript = "0x0101006a73161e57b24e5aa9e79588188ef6aa";
     }
