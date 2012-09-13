607756f70c123e319df84ad6b75bd37d53a631ec
Christian Glessner <cg@expertsinside.com>
2012-09-13 11:50:03 +0200

    Fixed issue with nuget nunit and JSON dll in Package

--- fetch_nuget_packages.ps1
+++ fetch_nuget_packages.ps1
@@ -1,2 +1,4 @@
+[System.Environment]::SetEnvironmentVariable('EnableNuGetPackageRestore', $true) 
+
 $packages = ls -Filter packages.config -recurse
 foreach($package in $packages) { ./tools/NuGet.exe i $package.FullName -o packages }
