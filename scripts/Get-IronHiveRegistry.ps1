4b513019c41de83f09f116f9f7f84e245ba310ae
Kevin Mees <kev.mees@gmail.com>
2012-10-08 21:08:20 +0200

    Fixed (encoding?) error in IronHiveRegistry script and moved it to a scripts folder

---
+++ scripts/Get-IronHiveRegistry.ps1
@@ -0,0 +1,17 @@
+function Global:Get-IronHiveRegistry
+{
+    Add-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction SilentlyContinue
+    [void][System.Reflection.Assembly]::LoadWithPartialName("IronSharePoint")
+    return [IronSharePoint.Administration.IronHiveRegistry]::Local
+}
+
+'Use Get-IronHiveRegistry to load the IronHiveRegistry'| out-host
+'Example:'| out-host
+'$target = Get-SPWebApplication "http://intranet"'| out-host
+'$hiveSite = Get-SPSite "http://intranet/sites/site"'| out-host
+'$ihr = Get-IronHiveRegistry'| out-host
+'$ihr.AddTrustedHive($hiveSite.Id)'| out-host
+'$ihr.TrustedHives'| out-host
+'$ihr.AddHiveMapping($hiveSite, $target)'| out-host
+'$ihr.HiveMappings' | out-host
+'$ihr.Update($true) ' | out-host
