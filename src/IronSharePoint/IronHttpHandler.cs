d62c2027f221082dac7356b576e82c45133321f9
Christian Glessner <cg@expertsinside.com>
2012-10-14 12:42:02 +0200

    HttpHanlder support added

---
+++ src/IronSharePoint/IronHttpHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using Microsoft.SharePoint;
+using System.IO;
+
+namespace IronSharePoint
+{
+    public class IronHttpHandler : IHttpHandler
+    {
+
+        private IHttpHandler _dynamicHandler;
+
+        public IHttpHandler DynamicHandler
+        {
+            get 
+            {
+                if (_dynamicHandler == null)
+                {
+                    var ironRuntime = IronRuntime.GetDefaultIronRuntime(SPContext.Current.Site);
+
+                    if (!String.IsNullOrEmpty(ironRuntime.HttpHandlerClass))
+                    {
+                        _dynamicHandler = ironRuntime.CreateDynamicInstance(ironRuntime.HttpHandlerClass) as IHttpHandler;
+  
+                    }
+                    else
+                    {
+
+                        throw new InvalidOperationException("No dynamic HttpHandlerFactory registered");
+                    }
+                }
+
+                return _dynamicHandler; 
+            }
+        }
+
+
+
+        public bool IsReusable
+        {
+            get { return DynamicHandler.IsReusable; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            DynamicHandler.ProcessRequest(context);
+        }
+
+    }
+}
