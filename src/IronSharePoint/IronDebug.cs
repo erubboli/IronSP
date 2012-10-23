6243b28fff9e4ad723f1ada96093b1049e0c1b11
Christian Glessner <cg@expertsinside.com>
2012-10-23 23:33:28 +0200

    Loca debug functionality added - local dev hive

---
+++ src/IronSharePoint/IronDebug.cs
@@ -0,0 +1,18 @@
+﻿#if DEBUG
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IronSharePoint
+{
+    public class IronDebug
+    {
+        public static string IronDevHivePah
+        {
+
+             get{ return System.Environment.GetEnvironmentVariable("IRONSPDEVHIVE").Replace("\\","//");}
+        }
+    }
+}
+#endif
