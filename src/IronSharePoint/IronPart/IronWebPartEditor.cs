2340ce0d3e56b1eb67c7a5d487dd58d753374c4b
Christian Glessner <cg@expertsinside.com>
2012-10-29 17:37:00 +0100

    IronPart SetDirty implemented

---
+++ src/IronSharePoint/IronPart/IronWebPartEditor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI.WebControls.WebParts;
+
+namespace IronSharePoint.IronPart
+{
+    public class IronWebPartEditor : EditorPart
+    {
+        private IronPart _ironPart;
+
+        
+
+
+        public override bool ApplyChanges()
+        {
+            
+        }
+
+        public override void SyncChanges()
+        {
+            
+        }
+    }
+}
