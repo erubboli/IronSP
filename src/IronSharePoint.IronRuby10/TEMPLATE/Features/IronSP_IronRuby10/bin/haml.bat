ebfacf7284897e759255bd52574efa6c1cc5d6b4
Kevin Mees <kev.mees@gmail.com>
2012-10-30 20:14:53 +0100

    Updated haml gem to version 3.0.25

--- src/IronSharePoint.IronRuby10/TEMPLATE/Features/IronSP_IronRuby10/bin/haml.bat
+++ src/IronSharePoint.IronRuby10/TEMPLATE/Features/IronSP_IronRuby10/bin/haml.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 IF NOT "%~f0" == "~f0" GOTO :WinNT
-@"ir.exe" "C:/Program Files (x86)/IronRuby 1.0/bin/haml" %1 %2 %3 %4 %5 %6 %7 %8 %9
+@"ir.exe" "Z:/code/IronSP/src/IronSharePoint.IronRuby10/TEMPLATE/Features/IronSP_IronRuby10/Lib/ironruby/gems/1.8/bin/haml" %1 %2 %3 %4 %5 %6 %7 %8 %9
 GOTO :EOF
 :WinNT
 @"ir.exe" "%~dpn0" %*
