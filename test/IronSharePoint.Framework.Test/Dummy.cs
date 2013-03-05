840c9c915f47f8192386ce89b84d8914b0aa924b
Kevin Mees <kev.mees@gmail.com>
2013-03-05 13:24:42 +0100

    Added NUnit, Moq and FluentAssertions to test project

---
+++ test/IronSharePoint.Framework.Test/Dummy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace IronSharePoint.Framework.Test
+{
+    [TestFixture]
+    class Dummy
+    {
+        [Test]
+        public void Test()
+        {
+            true.Should().BeTrue("works");
+        }
+    }
+}
