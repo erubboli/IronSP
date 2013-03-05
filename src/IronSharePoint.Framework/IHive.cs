97bcc81ce7f7aea2b2a9caa396c4aa6a8c2da376
Kevin Mees <kev.mees@gmail.com>
2013-03-05 12:35:47 +0100

    Added an interface for hives

---
+++ src/IronSharePoint.Framework/IHive.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+
+namespace IronSharePoint.Framework
+{
+    /// <summary>
+    /// Interface for a virtual folder that contains script files
+    /// </summary>
+    public interface IHive
+    {
+        /// <summary>
+        /// Checks if a file exists at the given <paramref name="path"/>.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns>TRUE if the file exists, FALSE otherwise</returns>
+        bool FileExists(string path);
+
+        /// <summary>
+        /// Checks if a folder exists at the given <paramref name="path"/>
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns>TRUE if the file exists, FALSE otherwise</returns>
+        bool DirectoryExists(string path);
+
+        /// <summary>
+        /// Opens a stream of the file at <paramref name="path"/> with write permissions
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns>A stream of the file with write permissions</returns>
+        Stream OpenInputFileStream(string path);
+
+        /// <summary>
+        /// Opens a stream of the file at <paramref name="path"/> with read permissions.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns>A stream of the file with read permissions</returns>
+        Stream OpenOutputFileSteam(string path);
+
+        /// <summary>
+        /// Transforms a relative path of the virtual folder to a full path
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns>The full path</returns>
+        string GetFullPath(string path);
+    }
+}
