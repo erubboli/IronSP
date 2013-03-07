faa971547083738dc9f34c6fed644d58dc15fbed
Kevin Mees <kev.mees@gmail.com>
2013-03-07 19:38:33 +0100

    Implemented String#ReplaceFirst extension method

---
+++ src/IronSharePoint.Framework/Util/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace IronSharePoint.Framework.Util
+{
+    public static class StringExtensions
+    {
+        public static string ReplaceFirst(this string source, string pattern, string substitute)
+        {
+            var index = source.IndexOf(pattern, StringComparison.InvariantCulture);
+            if (index < 0) return source;
+
+            return source.Substring(0, index) + substitute + source.Substring(index + pattern.Length);
+        }
+    }
+}
---
+++ test/IronSharePoint.Framework.Test/Util/StringExtensions_Fixture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using IronSharePoint.Framework.Util;
+using FluentAssertions;
+
+namespace IronSharePoint.Framework.Test.Util
+{
+    [TestFixture]
+    class StringExtensions_Fixture
+    {
+        [Test]
+        public void ReplaceFirst_ReplacesOnlyTheFirstMatch()
+        {
+            "bar bar baz".ReplaceFirst("bar", "foo").Should().Be("foo bar baz");
+        }
+    }
+}
