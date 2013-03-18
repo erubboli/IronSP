3b8ebb8c9d43963e8bf06a2270cde911bae96eba
Kevin Mees <kev.mees@gmail.com>
2013-03-18 15:13:10 +0100

    Refactored Fixture for IronScriptHost to be more readable

--- test/IronSharePoint.Framework.Test/IronScriptHost_Fixture.cs
+++ test/IronSharePoint.Framework.Test/IronScriptHost_Fixture.cs
@@ -1,49 +1,88 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using FluentAssertions;
 using IronSharePoint.Administration;
 using IronSharePoint.Hives;
 using NUnit.Framework;
 using TypeMock.ArrangeActAssert;
-using FluentAssertions;
 
-namespace IronSharePoint.Framework.Test
+// ReSharper disable CheckNamespace
+namespace IronSharePoint.Framework.Test.IronScriptHost_Fixture
+// ReSharper restore CheckNamespace
 {
     [TestFixture]
-    public class IronScriptHost_Fixture
+    public class IronScriptHost_FixtureBase
     {
-        IronScriptHost Sut;
+        [SetUp]
+        public void BaseSetUp()
+        {
+            HiveRegistry = HiveRegistry.Local;
+            SiteId = Guid.NewGuid();
+        }
+
+        public IronScriptHost Sut;
+        public HiveRegistry HiveRegistry;
+        public Guid SiteId;
 
         [TestFixtureSetUp]
         public void FixtureSetUp()
         {
             Isolate.Fake.StaticMethods<HiveRegistry>(Members.ReturnRecursiveFakes);
         }
+    }
 
+    public class GivenNoRegisteredHives : IronScriptHost_FixtureBase
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            HiveSetupCollection hiveSetups;
+            Isolate.WhenCalled(() => HiveRegistry.TryResolve(SiteId, out hiveSetups)).WillReturn(false);
+        }
 
         [Test]
-        public void Hive_WhenOneHiveSetup_CreatesHiveFromSetup()
+        public void Hive_ContainsOnlySystemHive()
         {
-            var siteId = Guid.NewGuid();
+            Sut = new IronScriptHost(SiteId);
+
+            Sut.Hive.Should().Contain(hive => hive is SystemHive);
+            Sut.Hive.Count().Should().Be(1);
+        }
+    }
+
+    public class GivenOneRegisteredHive : IronScriptHost_FixtureBase
+    {
+        [SetUp]
+        public void SetUp()
+        {
             var hiveSetups = new HiveSetupCollection
-                {
-                    new HiveSetup
-                        {
-                            HiveType = typeof (DirectoryHive),
-                            HiveArguments = new[] {"c:\\"}
-                        }
-                };
-            var registry = HiveRegistry.Local;
-            Isolate.WhenCalled(() => registry.TryResolve(siteId, out hiveSetups)).WillReturn(true);
+                    {
+                        new HiveSetup
+                            {
+                                HiveType = typeof (DirectoryHive),
+                                HiveArguments = new[] {"c:\\"},
+                                DisplayName = "Test Hive"
+                            }
+                    };
+            Isolate.WhenCalled(() => HiveRegistry.TryResolve(SiteId, out hiveSetups)).WillReturn(true);
+        }
 
-            Sut = new IronScriptHost(siteId);
+        [Test]
+        public void Hive_CreatesHiveFromSetup()
+        {
+            Sut = new IronScriptHost(SiteId);
 
-            Sut.Hive.Should().BeOfType<HiveComposite>();
-            (Sut.Hive as HiveComposite).Should().Contain(hive =>
-                                                           hive is DirectoryHive &&
-                                                           (hive as DirectoryHive).Root == "c:\\");
+            Sut.Hive.Should().Contain(hive => hive is DirectoryHive
+                                              && hive.Name == "Test Hive"
+                                              && (hive as DirectoryHive).Root == "c:\\");
+        }
+
+        [Test]
+        public void Hive_SystemHiveIsLast()
+        {
+            Sut = new IronScriptHost(SiteId);
+
+            Sut.Hive.Last().Should().BeOfType<SystemHive>();
         }
     }
 }
