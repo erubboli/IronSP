bacac47436410502de81bd6d98d221605a3d7794
Kevin Mees <kev.mees@gmail.com>
2013-04-07 01:34:23 +0200

    Fixed inspect for dynamic .net objects

--- src/IronSharePoint/TEMPLATE/Features/IronSP_Root/iron_sharepoint/patches/core/object.rb
+++ src/IronSharePoint/TEMPLATE/Features/IronSP_Root/iron_sharepoint/patches/core/object.rb
@@ -3,9 +3,9 @@ class Object
   alias_method :old_inspect, :inspect
 
   def inspect
-	if self.respond_to? "GetDynamicMemberNames".to_clr_string
-		old_inspect
-    if self.respond_to? :GetEnumerator
+    if self.respond_to? "GetDynamicMemberNames".to_clr_string
+      old_inspect
+    elsif self.respond_to? :GetEnumerator
       self.to_a.inspect
     else
       old_inspect
